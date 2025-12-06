c81158b904cd6272ed3c2c6e113d420f90d104c7
Vishwa Prasad L <vishwaprasad11@gmail.com>
2025-12-06 14:18:14 +0530

    est: dokploy github integration #2

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { Analytics } from "@vercel/analytics/react";
 
-// testing dokploy
+// testing dockploy
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
